6c773833f020ed8b91da30ea08a2b7175b5243ac
Irene Pavlenko <irenpavlenko22@gmail.com>
2024-07-29 22:53:41 -0400

    Hero styling and typing animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const Hero = () => {
+  const [displayedText, setDisplayedText] = useState("");
+  const fullText =
+    "Myy name is Irene, a web developer who crafts websites with the same joy and precision as a barista making the perfect latte.";
+  const typingSpeed = 60;
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typeText = () => {
+      if (currentIndex < fullText.length) {
+        setDisplayedText(prev => prev + fullText.charAt(currentIndex));
+        currentIndex++;
+        setTimeout(typeText, typingSpeed);
+      }
+    };
+
+    typeText();
+  }, []);
+
   return (
     <section id="hero">
-      <h1>Hello</h1>
+      <h1 class="lobster-regular">Hello!</h1>
       <p>
-        I'm Irene, a passionate Web Developer dedicated to creating
-        dynamic and user-friendly websites.
+        {displayedText}
       </p>
     </section>
   );
