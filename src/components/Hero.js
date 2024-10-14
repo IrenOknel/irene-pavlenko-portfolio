f6e3cfeeb3151a8bbb2584e2f2e3237a666504d3
Irene Pavlenko <irenpavlenko22@gmail.com>
2024-10-14 16:16:50 -0400

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,32 +4,37 @@ import "./App.css";
 const Hero = () => {
   const [displayedText, setDisplayedText] = useState("");
   const fullText =
-    "My name is Irene, a web developer who crafts websites with the same joy and precision as a barista making the perfect latte.";
+    "Myy name is Irene, a web developer who crafts websites with the same joy and precision as a barista making the perfect latte.";
   const typingSpeed = 60;
 
-  const currentIndexRef = useRef(0); // Keep track of current index
+  const currentIndexRef = useRef(0);
 
-  useEffect(() => {
-    const typeText = () => {
-      if (currentIndexRef.current < fullText.length) {
-        const nextText = fullText.substring(0, currentIndexRef.current + 1); 
-        setDisplayedText(nextText);
-        currentIndexRef.current++;
-        setTimeout(typeText, typingSpeed);
-      }
-    };
+  useEffect(
+    () => {
+      const typeText = () => {
+        if (currentIndexRef.current < fullText.length) {
+          const nextText = fullText.substring(0, currentIndexRef.current + 1);
+          setDisplayedText(nextText);
+          currentIndexRef.current++;
+          setTimeout(typeText, typingSpeed);
+        }
+      };
 
-    typeText();
+      typeText();
 
-    return () => {
-      currentIndexRef.current = fullText.length;
-    };
-  }, [fullText, typingSpeed]);
+      return () => {
+        currentIndexRef.current = fullText.length;
+      };
+    },
+    [fullText, typingSpeed]
+  );
 
   return (
     <section id="hero">
       <h1 className="lobster-regular">Hello!</h1>
-      <p>{displayedText}</p>
+      <p>
+        {displayedText}
+      </p>
     </section>
   );
 };
